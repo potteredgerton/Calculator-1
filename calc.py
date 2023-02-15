1f7f2ad4e054efaff1f44710db3d825ffaea186b
Potter J. Edgerton <106568662+potteredgerton@users.noreply.github.com>
2023-02-15 14:50:08 -0500

    Update calc.py
    Signed-off-by: Potter J. Edgerton <106568662+potteredgerton@users.noreply.github.com>

--- calc.py
+++ calc.py
@@ -7,7 +7,8 @@ def calculate(operator, num1, num2):
         return num1 * num2
     if (operator == /)
         return num1 / num2
-    
+ 
+
 while True:
     operator = input("What operation would you like to do?\nA = Add 2 numbers\nS = Subtract 2 numbers\nM = Multiply 2 numbers\nD = Divide 2 numbers\n")
     n1 = int(input("Number 1:\n"))
